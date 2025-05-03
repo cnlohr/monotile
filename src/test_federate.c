135ca302a0bb7f63eea355e9b7f2710fb1093939
cnlohr <lohr85@gmail.com>
2025-05-03 12:22:00 +0000

    Adding some testing

--- src/test_federate.c
+++ src/test_federate.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-char res[1024];
+
+// Test speed of chunked throughput
+// curl --raw -i https://cnvr.io/monotile/test_federate.cgi > /dev/null
+
+char res[16384];
+
 int main()
 {
 	printf( "Transfer-Encoding: chunked\r\n" );
 	printf( "Content-Type: application/octet-stream\r\n\r\n" );
 	fflush( stdout );
 	memset( res, 'x', sizeof( res ) );
-	for( int i = 0; i < 500000; i++ )
+	for( int i = 0; i < 50000; i++ )
 	{
 		write( 0, res, sizeof( res ) );
 		usleep(1);
 	}
+	write( 0, "done\n", 5); 
 	return 0;
 }
